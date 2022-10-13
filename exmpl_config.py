24e7285287231f0f7add645c9ddbfbca5cc99982
Igor Tankov <75366889a@gmail.com>
2022-10-13 12:57:39 +0300

    change example config

--- exmpl_config.py
+++ exmpl_config.py
@@ -1,3 +1,6 @@
 #rename this file to config.py
 tg_token = "123456:AABBCCaabbcc" # telegram token here
 
+#aliases
+cock_top_aliases = ("топ", "т", "top", "t") 
+cock_atop_aliases = ("антитоп", "атоп", "ат", "antitop", "atop", "at")
