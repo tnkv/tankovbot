96b0f0ca50a18cc7ff5efb1ca777f76137ded17e
Igor Tankov <75366889a@gmail.com>
2022-10-14 19:22:16 +0300

    blocked_ids to example config

--- exmpl_config.py
+++ exmpl_config.py
@@ -4,3 +4,5 @@ tg_token = "123456:AABBCCaabbcc" # telegram token here
 #aliases
 cock_top_aliases = ("топ", "т", "top", "t") 
 cock_atop_aliases = ("антитоп", "атоп", "ат", "antitop", "atop", "at")
+
+blocked_ids = [777000, 136817688]
